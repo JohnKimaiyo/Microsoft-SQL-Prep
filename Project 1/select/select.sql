d4c917459dee449e647f74a8ac11a70401dae17f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-30 16:08:11 +0300

    final

--- Project 1/select/select.sql
+++ Project 1/select/select.sql
@@ -2,3 +2,43 @@
 
 SELECT DISTINCT  Title
 FROM [AdventureWorksDW2019].[dbo].[DimEmployee]
+
+-- case statement -- 
+SELECT TOP (1000) [EmployeeKey]
+      ,[ParentEmployeeKey]
+      ,[EmployeeNationalIDAlternateKey]
+      ,[ParentEmployeeNationalIDAlternateKey]
+      ,[SalesTerritoryKey]
+      ,[FirstName]
+      ,[LastName]
+      ,[MiddleName]
+      ,[NameStyle]
+      ,[Title]
+      ,[HireDate]
+      ,[BirthDate]
+      ,[LoginID]
+      ,[EmailAddress]
+      ,[Phone]
+      ,[MaritalStatus]
+      ,[EmergencyContactName]
+      ,[EmergencyContactPhone]
+      ,[SalariedFlag]
+      ,[Gender]
+      ,[PayFrequency]
+      ,[BaseRate]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[SalesPersonFlag]
+      ,[DepartmentName]
+      ,[StartDate]
+      ,[EndDate]
+      ,[Status]
+      ,[EmployeePhoto],
+
+	  CASE
+
+	  WHEN VacationHours > 70 THEN 'Vacation hours ove rlimit' 
+	  ELSE 'Vacation Hours within Limit'
+	  END AS VacatioHourLimit
+  FROM [AdventureWorksDW2019].[dbo].[DimEmployee]
