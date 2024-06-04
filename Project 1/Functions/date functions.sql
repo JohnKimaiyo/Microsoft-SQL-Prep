635cc22f8bbdb23583f4bebf829da07bc0e6ad4f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-06-04 11:59:23 +0300

    final

--- Project 1/Functions/date functions.sql
+++ Project 1/Functions/date functions.sql
@@ -80,3 +80,46 @@ FROM (
     FROM [AdventureWorksDW2019].[dbo].[DimProduct]
 ) AS ProductWithMonth
 WHERE Extractmonth IS NOT NULL;
+
+SELECT *
+FROM (
+    SELECT TOP (1000) [ProductKey],
+          [ProductAlternateKey],
+          [ProductSubcategoryKey],
+          [WeightUnitMeasureCode],
+          [SizeUnitMeasureCode],
+          [EnglishProductName],
+          [SpanishProductName],
+          [FrenchProductName],
+          [StandardCost],
+          [FinishedGoodsFlag],
+          [Color],
+          [SafetyStockLevel],
+          [ReorderPoint],
+          [ListPrice],
+          [Size],
+          [SizeRange],
+          [Weight],
+          [DaysToManufacture],
+          [ProductLine],
+          [DealerPrice],
+          [Class],
+          [Style],
+          [ModelName],
+          [LargePhoto],
+          [EnglishDescription],
+          [FrenchDescription],
+          [ChineseDescription],
+          [ArabicDescription],
+          [HebrewDescription],
+          [ThaiDescription],
+          [GermanDescription],
+          [JapaneseDescription],
+          [TurkishDescription],
+          [StartDate],
+          [EndDate],
+          [Status],
+          datename(month,EndDate) AS Extractmonth
+    FROM [AdventureWorksDW2019].[dbo].[DimProduct]
+) AS ProductWithMonth
+WHERE Extractmonth IS NOT NULL;
