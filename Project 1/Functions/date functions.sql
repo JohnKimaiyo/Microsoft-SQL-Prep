639305cd54ccd78804305f525845bf8d27c2b422
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-06-04 10:56:25 +0300

    final

--- Project 1/Functions/date functions.sql
+++ Project 1/Functions/date functions.sql
@@ -37,3 +37,46 @@ SELECT TOP (1000) [ProductKey]
 	  datediff(yyyy,EndDate, StartDate) AS diffDate
   FROM [AdventureWorksDW2019].[dbo].[DimProduct]
   WHERE EndDate IS NOT NULL;
+
+  SELECT *
+FROM (
+    SELECT TOP (1000) [ProductKey],
+          [ProductAlternateKey],
+          [ProductSubcategoryKey],
+          [WeightUnitMeasureCode],
+          [SizeUnitMeasureCode],
+          [EnglishProductName],
+          [SpanishProductName],
+          [FrenchProductName],
+          [StandardCost],
+          [FinishedGoodsFlag],
+          [Color],
+          [SafetyStockLevel],
+          [ReorderPoint],
+          [ListPrice],
+          [Size],
+          [SizeRange],
+          [Weight],
+          [DaysToManufacture],
+          [ProductLine],
+          [DealerPrice],
+          [Class],
+          [Style],
+          [ModelName],
+          [LargePhoto],
+          [EnglishDescription],
+          [FrenchDescription],
+          [ChineseDescription],
+          [ArabicDescription],
+          [HebrewDescription],
+          [ThaiDescription],
+          [GermanDescription],
+          [JapaneseDescription],
+          [TurkishDescription],
+          [StartDate],
+          [EndDate],
+          [Status],
+          MONTH(EndDate) AS Extractmonth
+    FROM [AdventureWorksDW2019].[dbo].[DimProduct]
+) AS ProductWithMonth
+WHERE Extractmonth IS NOT NULL;
